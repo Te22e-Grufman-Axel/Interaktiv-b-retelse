9e8fdd40d6c24ee6ebc5d022e270b62b3daab2f1
Axel Grufman <Axel.Grufman@elev.ga.ntig.se>
2023-09-13 10:54:19 +0200

    Funny

--- Interaktiv bärätelse/Program.cs
+++ Interaktiv bärätelse/Program.cs
@@ -1,5 +1,14 @@
-﻿while (true)
+﻿int dörr = 0;
+
+
+
+while (true)
 {
+
+
+
+
+
     String choice = "";
 
     Console.ForegroundColor = ConsoleColor.Yellow;
@@ -33,6 +42,8 @@
 
     if (choice == "h") // första valet
     {
+        Console.WriteLine("");
+        Console.WriteLine("");
         Console.WriteLine("Du kommer längre in i huset");
         Console.WriteLine("Framför dig har du 2 dörrar");
         Console.WriteLine("Men elden är nära så du kan bara välja en"); //Vad som händer effter första valet
@@ -44,14 +55,19 @@
 
         if (choice == "h")//fler val
         {
+            Console.WriteLine("");
+            Console.WriteLine("");
             Console.WriteLine("Du springer in i dörren till höger");
             Console.WriteLine("Där inne ser du din son,din dotter och din fru.");//mer val text
             Console.WriteLine("Du bär ut din son och din dotter medans din fru går ut själv");
             Console.WriteLine("Du lyckas rädda dom och ses som en hjälte i din familjs ögon");
+            Console.WriteLine("");
             Console.WriteLine("Klick på en knapp för att köra igen");
         }
         else if (choice == "v")//fler val
         {
+            Console.WriteLine("");
+            Console.WriteLine("");
             Console.WriteLine("Du väljer att låta din familj brinna inne och dö en plågsam död");
             Console.WriteLine("Men du hinner ta din dator och springa ut ur huset med den");
             Console.WriteLine("Medans du springer ut hör du din fru och barn ropa dit namn och be om att bli räddade");
@@ -60,16 +76,22 @@
             Console.WriteLine("Du glömde att dom bodde på andra sidan gatan"); //mer val text
             Console.WriteLine("När dom ser dig springa ut ur huset med din dator och inte din familj blir dom så arga att dom slår sönder din dator");
             Console.WriteLine("Du försöker kämpa emot men det leder till att dom slår ihjäl dig också");
+            Console.WriteLine("");
             Console.WriteLine("Klick på en knapp för att köra igen");
         }
         else // vad som händer om du skriver fel
         {
+            Console.WriteLine("");
+            Console.WriteLine("");
             Console.WriteLine("Du valde inget rätt val så du och din familj brann upp");
+            Console.WriteLine("");
             Console.WriteLine("Klick på en knapp för att köra igen");
         }
     }
     else if (choice == "v")//fler val
     {
+        Console.WriteLine("");
+        Console.WriteLine("");
         Console.WriteLine("Du väljer att springa ut ur huset");
         Console.WriteLine("Väl ute ser du att din frus föräldrar står där");
         Console.WriteLine("Dom ber dig springa in och rädda din fru och dina barn");//mer val text
@@ -79,42 +101,123 @@
 
         if (choice == "u")//fler val
         {
+            Console.WriteLine("");
+            Console.WriteLine("");
             Console.WriteLine("Du väljer att stanna ute och ser hur din familj brinner upp");//mer val text
             Console.WriteLine("Din frus föräldrar anklagar dig och ett par dar effter branden tar du självmord");
+            Console.WriteLine("");
             Console.WriteLine("Klick på en knapp för att köra igen");
         }
         else if (choice == "i")//fler val
         {
+            Console.WriteLine("");
+            Console.WriteLine("");
             Console.WriteLine("Du springer in igen");
             Console.WriteLine("När du kommer till rummet dom är i är det för sent");
             Console.WriteLine("Dom är redan döda av röken i rummet"); //mer val text
             Console.WriteLine("Du försöker ta dig ut men det är för sent för dig också");
             Console.WriteLine("Du faller ihop av röken och dör");
+            Console.WriteLine("");
+            Console.WriteLine("Klicka på en knapp för att köra igen");
         }
         else// vad som händer om du skriver fel
         {
+            Console.WriteLine("");
+            Console.WriteLine("");
             Console.WriteLine("Du valde inget rätt val så din familj brann upp");
             Console.WriteLine("Och din frus föräldrar dödar dig");
+            Console.WriteLine("");
             Console.WriteLine("Klick på en knapp för att köra igen");
         }
     }
     else if (choice == "f")
     {
-        Console.WriteLine("Du slår upp dörren och springer in");
-        Console.WriteLine("Du ser att det är en trappa och kommer på att det bara är trappan ner till källaren");
-        Console.WriteLine("Men det är för sent och du rammlar ner och dör");
-        Console.WriteLine("Klick på en knapp för att köra igen");
-
-
+        if (dörr == 0)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Du slår upp dörren och springer in");
+            Console.WriteLine("Du ser att det är en trappa och kommer på att det bara är trappan ner till källaren");
+            Console.WriteLine("Men det är för sent och du rammlar ner och dör");
+            Console.WriteLine("");
+            Console.WriteLine("Klick på en knapp för att köra igen");
+            dörr++;
+        }
+        else if (dörr == 1)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Varför går du in här igen????");
+            Console.WriteLine("Börja om och gå inte in här");
+            Console.WriteLine("Du dör ju bara");
+            Console.WriteLine("");
+            Console.WriteLine("Klick på en knapp för att köra igen");
+            dörr++;
+        }
+        else if (dörr == 2)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Jag sa ju åt dig att sluta gå in här");
+            Console.WriteLine("Jag varnar dig");
+            Console.WriteLine("Om du går in här kommer du inte gilla det");
+            Console.WriteLine("");
+            Console.WriteLine("Klick på en knapp för att köra igen");
+            dörr++;
+        }
+        else if (dörr == 3)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("VARFÖR GÅR DU IN HÄR!!!!!");
+            Console.WriteLine("JAG SA ATT DU INTE SKULLE GILLA DET");
+            Console.WriteLine("Sista chansen");
+            Console.WriteLine("Om du går in här kommer du inte GILLA DET!!!!!!!");
+            Console.WriteLine("");
+            Console.WriteLine("Klick på en knapp för att köra igen");
+            dörr++;
+        }
+        else
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Jag ger upp");
+            Console.WriteLine("Varför går du in???");
+            Console.WriteLine("SNälla prata bara med mig");
+            Console.WriteLine("varför pågar du mig???");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Du kommer längre in i rummet");
+            Console.WriteLine("DU ser att narator har tagit självmord");
+            Console.WriteLine("DU set hans kropp och försöker fly");
+            Console.WriteLine("Men det är för sent och du och din familj brinner upp");
+            Console.WriteLine("");
+            Console.WriteLine("Klicka på en knapp för att köra igen");
+            dörr++;
+        }
     }
     else// vad som händer om du skriver fel
     {
         Console.WriteLine("Du valde inget rätt val så du och din familj brann upp");
+        Console.WriteLine("");
         Console.WriteLine("Klick på en knapp för att köra igen");
     }
+Console.ReadKey();
+Console.Clear();
+if(dörr > 4)
+{
+Console.WriteLine("Du gick in i dörren du inte skulle gå igenom " + dörr + " Gånger" );
+Console.WriteLine("VARFÖR????");
+Console.WriteLine("");
+Console.WriteLine("Tryck på en knapp för att köra igen");
+}
 
+ 
 
-
-    Console.ReadKey();//så texten inte försviner direkt
-    Console.Clear();
 }
